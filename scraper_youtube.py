692b3adf7f78aaab3e8831dc9b69256be3c5dcf0
0xsky <103289721+tegu03@users.noreply.github.com>
2025-06-26 19:10:20 +0700

    Create scraper_youtube.py

---
+++ scraper_youtube.py
@@ -0,0 +1,6 @@
+CHANNELS = {
+    "andy": "https://www.youtube.com/@AndySenjaya",
+    "sulianto": "https://www.youtube.com/@suliantoindriaputra",
+    "khun": "https://www.youtube.com/@khuncoin",
+    "rizki": "https://www.youtube.com/@RizkiAditama"
+}
